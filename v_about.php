4b9f444c144068464248f96c763a8423c7c3f958
mochsinggih <91039748+mochsinggih@users.noreply.github.com>
2021-10-06 19:05:43 +0700

    Pertemuan 3
    Menambahkan Web.php v_footer.php v_header.php v_index.php v_about.php stylebuku.css

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+<h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Singgih</li>
+ <li>Nama Belakang : Permadi</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Kapuk Swadaya No.9 jakarta barat</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Basket</li>
+ <li>Sepak Bola</li>
+ </ul>
+</section>
